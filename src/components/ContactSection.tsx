f2883a03c31ee7f405051065b4eb3a7a7be107ef
kananmittal <kananmittal05122005@gmail.com>
2026-05-18 01:20:43 +0530

    Add Incubation Centre IIIT Delhi address to ContactSection

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -64,8 +64,13 @@ export default function ContactSection() {
                   </svg>
                   +91 80855 22102
                 </p>
-
-
+                <p className="flex items-start text-gray-700">
+                  <svg className="w-6 h-6 mr-3 text-primary flex-shrink-0 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                  </svg>
+                  Incubation Centre, IIIT Delhi
+                </p>
               </div>
             </div>
             {/* Enhanced Form */}
