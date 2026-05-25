d6f89dbc708b6ab6e209959e7377426977340d6a
kananmittal <kananmittal05122005@gmail.com>
2026-05-26 01:59:05 +0530

    Update address to IIITD Innovation and Incubation Center

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -69,7 +69,11 @@ export default function ContactSection() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                   </svg>
-                  Incubation Centre, IIIT Delhi
+                  <span className="flex flex-col">
+                    <span>IIITD Innovation and Incubation Center</span>
+                    <span>535 , 5th Floor Okhla Industrial Estate , Phase-III</span>
+                    <span>New Delhi-110020</span>
+                  </span>
                 </p>
               </div>
             </div>
